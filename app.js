4760e6eca496d37ceab7de77f187991741ca2d29
Coding-Dore <lkjuio3865@naver.com>
2022-02-01 08:47:41 +0900

    before sql

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs')//render로 불러올 파일의 확장자는 ejs
 app.set('views','./views')//view engine이 가져올 resource?들이 위치한 경로
 app.use('/public',express.static(__dirname+'/public'))//public폴더를 url을 통해 접근할 수 있게 해줌
 
-app.use(helmet())//보안
+//app.use(helmet())//보안
 
 app.use(express.json())//body를 사용할 수 있게 해줌
 app.use(express.urlencoded())
@@ -29,11 +29,11 @@ app.use('/', mainRouter)
 
 app.use('/',codeRouter)
 
-let ip = '59.6.104.50'
+let ip = '192.168.219.101'
 
 app.listen(3000,ip, function(){
     let ts = Date.now();
-    let date_ob = new Date(ts);
+    let date_ob = new Date(ts)
     console.log(date_ob)
     db.mongoose.connect('mongodb://localhost:27017/CodeArchive')
     console.log("Server is running on 3000")
